86232fea4641d043b9e32ecb41ff74938352ed20
Antonio Navarro <Antonio Navarro@DESKTOP-JOFT7M8>
2022-09-29 18:22:15 +0200

    Finalizado

--- EjerciciosTema1/src/ejercicio1/Ejercicio1.java
+++ EjerciciosTema1/src/ejercicio1/Ejercicio1.java
@@ -7,21 +7,23 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// Declaramos el numero a redondear, al ser un numero con decimales va a ser double o float..
+		// Declaramos la variable para el numero a redondear, al ser un numero con decimales va a ser double o float..
 		double num;
 		
 		// Creamos la variable para poder utilizar el Scanner y cambiamos su formato al Formato US
 		final Scanner read = new Scanner(System.in);
 		read.useLocale(Locale.US);
 		
-		// Pedimos al usuario que introduzca un numero con decimales
+		// Pedimos al usuario que introduzca un numero con decimales y lo guardamos en la variable num
 		System.out.print("Introduce un numero con decimales: ");
 		num = read.nextDouble();
 		
-		// Sumamos 0.5 a nuestro numero para que de esta manera nos devuelva el numero entero al cual se encuentra mas cerca.
+		// Sumamos 0.5 a nuestro numero de esta forma si el numero vale X.5 o mas nos lo va a redondear a su numero mas proximo y no a simplimente la unidad
 		num += 0.5d;
 		
-		// Imprimimos el resultado por pantalla cambiandolo al tipo int
+		System.out.println();System.out.println("---------------------------------------");System.out.println();
+		
+		// Imprimimos el resultado por pantalla cambiandolo al tipo int.
 		System.out.println("El numero redondeado es: " + (int) num);
 		
 		// Cerramos el Scanner
