02e85534c12a7974b10722c503f21deec781efb1
Antonio Navarro <Antonio Navarro@DESKTOP-JOFT7M8>
2022-09-29 18:55:14 +0200

    Finalizado

--- EjerciciosTema1/src/ejercicio5/Ejercicio5.java
+++ EjerciciosTema1/src/ejercicio5/Ejercicio5.java
@@ -10,13 +10,13 @@ public class Ejercicio5 {
 		// Creamos la variable a, b, c y x las cuales van a tomar un valor por el usuario y la variable 'y' que va a ser el resultado de la ecuación
 		double a, b, c, x, y;
 		
-		// Creamos y configuramos el Scanner a la Región US
+		// Creamos el Scanner en la variables read y asignamos el formato US
 		final Scanner read = new Scanner(System.in);
 		read.useLocale(Locale.US);
 		
-		// Enseñamos al usuario la ecuación a resolver
+		// Mostramos por pantalla la ecuación a resolver
 		System.out.println("y = ax^2 + bx + c");
-		System.out.println();
+		System.out.println();System.out.println("---------------------------------------");System.out.println();
 		
 		// Pedimos al Usuario que indique los valores de a, b, c y x
 		System.out.print("Introduce el valor de 'a': ");
@@ -30,6 +30,14 @@ public class Ejercicio5 {
 		
 		// Calculamos la Ecuación y asignamos su valor a 'y'
 		y = a * (x * x) + b * x + c;
+		
+		System.out.println();System.out.println("---------------------------------------");System.out.println();
+		
+		// Imprimimos el resultado por pantalla
+		System.out.println(a + " · " + x + "^2 + " + b + " · " + x + " + " + c + " = " + y);
+		
+		// Cerramos el Scanner
+		read.close();
 
 	}
 
