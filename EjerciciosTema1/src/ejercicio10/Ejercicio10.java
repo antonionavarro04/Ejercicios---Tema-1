3561ae11bb8c60f2009bbddf9ef5ea31c80c39af
navarro41yt <anavarro41yt@gmail.com>
2022-10-02 18:22:49 +0200

    Creacion del Ejercicio

---
+++ EjerciciosTema1/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,18 @@
+package ejercicio10;
+
+//Importamos la libreria Scanner y Locale
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		// Declaramos la variable lanzamiento la cual almacenará la distancia del lanzamiento en m, esta distancia sera introducida por el Usuario
+		
+		// Creamos el Scanner en la variables read y asignamos el formato US
+		final Scanner read = new Scanner(System.in);
+		read.useLocale(Locale.US);
+
+	}
+
+}
